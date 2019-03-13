6544eb92102129745a0b98882c4f6ddc290b3f60
psi <psi@7io.org>
2019-03-13 17:39:57 +0900

    検索エンジンのクローラーを悪用したSPAMがやってくるので、インデックスしないようにお願いした https://7io.org/2019/03/13/17:39:44/

--- header.php
+++ header.php
@@ -29,12 +29,17 @@
     echo " $site_description";
 
   // Add a page number if necessary:
-  if ( $paged >= 2 || $page >= 2 )
+  if ( $paged >= 2 || $page >= 2 ) {
     echo ' | ' . sprintf( __( 'Page %s', 'scrappy' ), max( $paged, $page ) );
-
+  }
   ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+if (is_search()) {
+  echo '<meta name="robots" content="noindex,follow" />';
+}
+?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
